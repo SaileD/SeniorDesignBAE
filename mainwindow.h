f3b27bd40d2dc31314a034bd48953e820eb66fc2
Saile Daimwood <sailedaimwood@Sailes-MacBook-Pro.local>
2018-02-08 16:38:52 -0700

    Cleaned code up a lot

--- mainwindow.h
+++ mainwindow.h
@@ -15,61 +15,22 @@ class MainWindow : public QMainWindow
 public:
     MainWindow();
 
-protected:
-#ifndef QT_NO_CONTEXTMENU
-    void contextMenuEvent(QContextMenuEvent *event) override;
-#endif // QT_NO_CONTEXTMENU
-
 private slots:
     void newFile();
     void open();
     void save();
-    void print();
-    void undo();
-    void redo();
-    void cut();
-    void copy();
-    void paste();
-    void bold();
-    void italic();
-    void leftAlign();
-    void rightAlign();
-    void justify();
-    void center();
-    void setLineSpacing();
-    void setParagraphSpacing();
     void about();
-    void aboutQt();
 
 private:
     void createActions();
     void createMenus();
 
     QMenu *fileMenu;
-    QMenu *editMenu;
-    QMenu *formatMenu;
     QMenu *helpMenu;
-    QActionGroup *alignmentGroup;
     QAction *newAct;
     QAction *openAct;
     QAction *saveAct;
-    QAction *printAct;
-    QAction *exitAct;
-    QAction *undoAct;
-    QAction *redoAct;
-    QAction *cutAct;
-    QAction *copyAct;
-    QAction *pasteAct;
-    QAction *boldAct;
-    QAction *italicAct;
-    QAction *leftAlignAct;
-    QAction *rightAlignAct;
-    QAction *justifyAct;
-    QAction *centerAct;
-    QAction *setLineSpacingAct;
-    QAction *setParagraphSpacingAct;
     QAction *aboutAct;
-    QAction *aboutQtAct;
     QLabel *infoLabel;
     QLabel *altitudeInfo_1;
     QLabel *latitudeInfo_1;
