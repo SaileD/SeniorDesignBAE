e5cac72e729bea8772092fcee48e95b468963bb0
Saile Daimwood <sailedaimwood@Sailes-MacBook-Pro.local>
2018-02-08 16:22:30 -0700

    Basic Mockup for a single drone

--- mainwindow.h
+++ mainwindow.h
@@ -2,22 +2,81 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
-#include <QPushButton>
 
-namespace Ui {
-class MainWindow;
-}
+class QAction;
+class QActionGroup;
+class QLabel;
+class QMenu;
 
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
 
 public:
-    explicit MainWindow(QWidget *parent = 0);
+    MainWindow();
+
+protected:
+#ifndef QT_NO_CONTEXTMENU
+    void contextMenuEvent(QContextMenuEvent *event) override;
+#endif // QT_NO_CONTEXTMENU
+
+private slots:
+    void newFile();
+    void open();
+    void save();
+    void print();
+    void undo();
+    void redo();
+    void cut();
+    void copy();
+    void paste();
+    void bold();
+    void italic();
+    void leftAlign();
+    void rightAlign();
+    void justify();
+    void center();
+    void setLineSpacing();
+    void setParagraphSpacing();
+    void about();
+    void aboutQt();
 
 private:
-    Ui::MainWindow *ui;
-    QPushButton *butt;
+    void createActions();
+    void createMenus();
+
+    QMenu *fileMenu;
+    QMenu *editMenu;
+    QMenu *formatMenu;
+    QMenu *helpMenu;
+    QActionGroup *alignmentGroup;
+    QAction *newAct;
+    QAction *openAct;
+    QAction *saveAct;
+    QAction *printAct;
+    QAction *exitAct;
+    QAction *undoAct;
+    QAction *redoAct;
+    QAction *cutAct;
+    QAction *copyAct;
+    QAction *pasteAct;
+    QAction *boldAct;
+    QAction *italicAct;
+    QAction *leftAlignAct;
+    QAction *rightAlignAct;
+    QAction *justifyAct;
+    QAction *centerAct;
+    QAction *setLineSpacingAct;
+    QAction *setParagraphSpacingAct;
+    QAction *aboutAct;
+    QAction *aboutQtAct;
+    QLabel *infoLabel;
+    QLabel *altitudeInfo_1;
+    QLabel *latitudeInfo_1;
+    QLabel *longitudeInfo_1;
+    QLabel *velocityInfo_1;
+    QLabel *batteryInfo_1;
+    QLabel *flightInfo_1;
 };
 
-#endif // MAINWINDOW_H
+#endif
